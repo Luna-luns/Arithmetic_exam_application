ab169f7524fada403aeab6779d7676b1325207fc
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-07-28 19:03:11 +0300

    Stage 3

--- arithmetic.py
+++ arithmetic.py
@@ -3,10 +3,22 @@ from exercise import Exercise
 from user_answer import UserAnswer
 
 
-exercise = Exercise()
-task = exercise.generate_task()
-ui.print_task(task)
-exercise.execute_task()
-user_answer = UserAnswer(ui.get_answer())
-result = exercise.check_answer(user_answer.answer)
-ui.print_result(result)
+count_tasks = 0
+count_correct = 0
+
+while count_tasks < 5:
+    exercise = Exercise()
+    task = exercise.generate_task()
+    ui.print_task(task)
+    exercise.execute_task()
+
+    user_answer = UserAnswer(ui.get_answer())
+    result = exercise.check_answer(user_answer.answer)
+    ui.print_result(result)
+
+    count_tasks += 1
+
+    if result == 'Right!':
+        count_correct += 1
+
+ui.print_correct_number(count_correct)
--- ui.py
+++ ui.py
@@ -3,8 +3,19 @@ def print_task(task: str) -> None:
 
 
 def get_answer() -> int:
-    return int(input().strip())
+    result = None
+    while True:
+        try:
+            result = int(input().strip())
+            break
+        except ValueError:
+            print('Incorrect format.')
+    return result
 
 
 def print_result(result: str) -> None:
     print(result)
+
+
+def print_correct_number(num: int) -> None:
+    print(f'Your mark is {num}/5.')
